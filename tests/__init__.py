8b6491a32e036299a412487fb5a0a58693173ba2
Artem Nechai <lenikol95@mail.ru>
2021-12-27 22:51:00 +0300

    create test package

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""This is a testing package of the project."""
---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+"""This is a main testing module of the project."""
+
+
+def test_something():
+    """Empty test."""
+    pass  # noqa: WPS420
