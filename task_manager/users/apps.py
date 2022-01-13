bafed14cf73030a9e1ad96867299e8f40afb413c
Артём Нечай <a.nechai@okenit.ru>
2022-01-13 10:29:00 +0300

    add docstring for app

--- task_manager/users/apps.py
+++ task_manager/users/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """Application config."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'task_manager.users'
