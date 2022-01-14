7258c24264237e2975ed7f9e50f05923fea1bdfd
Артём Нечай <a.nechai@okenit.ru>
2022-01-14 10:15:00 +0300

    change routing logic

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -16,14 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from task_manager.views import LoginView, MainPageView
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', MainPageView.as_view(), name='main'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('users/', include('task_manager.users.urls')),
+    path('', include('task_manager.users.urls')),
     path('labels/', include('task_manager.labels.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
     path('tasks/', include('task_manager.tasks.urls')),
+    path('admin/', admin.site.urls),
 ]
--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from task_manager.users.views import UsersListView
+from task_manager.users.views import MainPageView, LoginView, UsersListView
 
 urlpatterns = [
-    path('', UsersListView.as_view(), name='users'),
+    path('', MainPageView.as_view(), name='main'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('users/', UsersListView.as_view(), name='users'),
 ]
