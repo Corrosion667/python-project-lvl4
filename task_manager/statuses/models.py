5c00e4ad7cf5459ae6397ac64b97083db45f492d
Artem Nechai <lenikol95@mail.ru>
2022-01-16 00:03:44 +0300

    create model for statuses

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(
+        verbose_name=_('Name'), max_length=50, unique=True,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name=_('Creation date'),
+    )
+
+    class Meta:
+        verbose_name = _('status')
+        verbose_name_plural = _('statuses')
+
+    def __str__(self):
+        return self.name
