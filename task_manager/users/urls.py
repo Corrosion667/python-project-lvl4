7e97db6f75c4d5f105f2dfd133d28cfb299d57e6
Artem Nechai <lenikol95@mail.ru>
2022-01-15 18:22:38 +0300

    update urls

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from task_manager.users.views import LoginView, MainPageView, UsersListView
+from task_manager.users.views import LoginView, MainPageView, UsersListView, SignupView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main'),
+    path('users/create/', SignupView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LoginView.as_view(), name='logout'),
     path('users/', UsersListView.as_view(), name='users'),
 ]
