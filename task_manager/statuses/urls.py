c886e1a0372a2e909a4cca5859aec9082fd08e3c
Artem Nechai <lenikol95@mail.ru>
2022-01-15 23:42:13 +0300

    create urls for statuses

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from task_manager.statuses import views
+from task_manager.statuses.views import (
+    StatusesListView,
+    CreateStatusView,
+    UpdateStatusView,
+    DeleteStatusView,
+)
 
 urlpatterns = [
-    path('', views.main, name='statuses'),
+    path('', StatusesListView.as_view(), name='statuses'),
+    path('create/', CreateStatusView.as_view(), name='create_status'),
+    path('<int:pk>/update/', UpdateStatusView.as_view(), name='update_status'),
+    path('<int:pk>/delete/', DeleteStatusView.as_view(), name='delete_status'),
 ]
