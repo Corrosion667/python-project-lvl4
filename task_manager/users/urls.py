717806c75bdc73ba89138955ef1b3786e343204c
Artem Nechai <lenikol95@mail.ru>
2022-01-15 22:07:34 +0300

    add update user function

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,17 +2,19 @@ from django.urls import path
 
 from task_manager.users.views import (
     MainPageView,
+    UsersListView,
     SignupView,
+    UpdateUserView,
     UserLoginView,
     UserLogoutView,
-    UsersListView,
+
 )
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main'),
     path('users/', UsersListView.as_view(), name='users'),
     path('users/create/', SignupView.as_view(), name='signup'),
-    path('users/<int:pk>/update/', SignupView.as_view(), name='update_user'),
+    path('users/<int:pk>/update/', UpdateUserView.as_view(), name='update_user'),
     path('users/<int:pk>/delete/', SignupView.as_view(), name='delete_user'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, ListView
+from django.views.generic import CreateView, TemplateView, ListView, UpdateView
 
 from task_manager.users.forms import SignupForm
 from task_manager.users.models import User
@@ -12,12 +12,30 @@ class MainPageView(TemplateView):
     template_name = 'main_page.html'
 
 
+class UsersListView(ListView):
+    """View for users page."""
+
+    template_name = 'users.html'
+    context_object_name = 'users_list'
+
+    def get_queryset(self):
+        """Return users list."""
+        return User.objects.all()
+
+
 class SignupView(CreateView):
     """View for signup page."""
 
     model = User
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+    form_class = SignupForm
+
+
+class UpdateUserView(UpdateView):
+    model = User
+    template_name = 'update_user.html'
+    next_page = reverse_lazy('users')
     form_class = SignupForm
 
 
@@ -31,14 +49,3 @@ class UserLoginView(LoginView):
 class UserLogoutView(LogoutView):
 
     next_page = reverse_lazy('main')
-
-
-class UsersListView(ListView):
-    """View for users page."""
-
-    template_name = 'users.html'
-    context_object_name = 'users_list'
-
-    def get_queryset(self):
-        """Return users list."""
-        return User.objects.all()
