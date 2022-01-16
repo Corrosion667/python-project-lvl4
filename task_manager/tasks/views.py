054ce178b8f735dd74698a10df4b4a492385d0f4
Artem Nechai <lenikol95@mail.ru>
2022-01-17 01:26:24 +0300

    update view for create task func

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -2,7 +2,7 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import CreateView, ListView
 
 from task_manager.tasks.forms import CreateForm
 from task_manager.tasks.models import Task
@@ -32,3 +32,8 @@ class CreateTaskView(LoginRequiredMixin, CreateView):
     template_name = 'create_task.html'
     form_class = CreateForm
     login_url = 'login'
+
+    def form_valid(self, form):
+        """Set author of task as active user."""  # noqa: DAR201
+        form.instance.author = self.request.user
+        return super().form_valid(form)
