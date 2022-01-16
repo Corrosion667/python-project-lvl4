ee16e2864aa5c3e831dd2c4196f1971c9c7e7fd0
Artem Nechai <lenikol95@mail.ru>
2022-01-17 00:05:15 +0300

    add task creation form

---
+++ task_manager/tasks/forms.py
@@ -0,0 +1,25 @@
+"""Forms for tasks app."""
+
+from django.forms import ModelForm
+
+from task_manager.tasks.models import Task
+
+
+class CreateForm(ModelForm):
+    """Form for create tasks page."""
+
+    class Meta(object):
+        """Meta information of form."""
+
+        model = Task
+        fields = [
+            'name',
+            'description',
+            'status',
+            'executor',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].required = True
+        self.fields['status'].required = True
