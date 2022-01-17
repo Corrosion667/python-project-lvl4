52cdf57d7ab4d0b4cc6028e8c9078c467c83b420
Artem Nechai <lenikol95@mail.ru>
2022-01-17 22:24:06 +0300

    add str method to user objects

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -6,4 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """A task manager user."""
 
-    pass
+    def __str__(self):
+        """Present object as a string.
+
+        Returns:
+            Full name of the user.
+        """
+        return self.get_full_name()
