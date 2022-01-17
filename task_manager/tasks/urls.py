8a2ad8886e67fb1d3397aa393561dbdccc65bcaa
Artem Nechai <lenikol95@mail.ru>
2022-01-17 22:38:27 +0300

    add url task details

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from task_manager.tasks.views import (
     CreateTaskView,
     DeleteTaskView,
+    TaskDetailsView,
     TasksListView,
     UpdateTaskView,
 )
@@ -14,4 +15,5 @@ urlpatterns = [
     path('create/', CreateTaskView.as_view(), name='create_task'),
     path('<int:pk>/update/', UpdateTaskView.as_view(), name='update_task'),
     path('<int:pk>/delete/', DeleteTaskView.as_view(), name='delete_task'),
+    path('<int:pk>/', TaskDetailsView.as_view(), name='task_details'),
 ]
