8019252f535581fa6742c25ef5d6b01dcf2b5b98
Artem Nechai <lenikol95@mail.ru>
2022-01-17 21:42:21 +0300

    add success messages for statuses

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,6 +1,8 @@
 """Module with views logic of the statuses app."""
 
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from task_manager.statuses.forms import CreateForm
@@ -24,30 +26,33 @@ class StatusesListView(CustomLoginMixin, ListView):
         return Status.objects.all()
 
 
-class CreateStatusView(CustomLoginMixin, CreateView):
+class CreateStatusView(SuccessMessageMixin, CustomLoginMixin, CreateView):
     """View for create status page."""
 
     model = Status
     success_url = reverse_lazy('statuses')
+    success_message = _('Status successfully created')
     template_name = 'create_status.html'
     form_class = CreateForm
     login_url = 'login'
 
 
-class UpdateStatusView(CustomLoginMixin, UpdateView):
+class UpdateStatusView(SuccessMessageMixin, CustomLoginMixin, UpdateView):
     """View for change status name page."""
 
     model = Status
     success_url = reverse_lazy('statuses')
+    success_message = _('Status successfully changed')
     template_name = 'update_status.html'
     form_class = CreateForm
     login_url = 'login'
 
 
-class DeleteStatusView(CustomLoginMixin, DeleteView):
+class DeleteStatusView(SuccessMessageMixin, CustomLoginMixin, DeleteView):
     """View for status deletion page."""
 
     model = Status
     template_name = 'delete_status.html'
     success_url = reverse_lazy('statuses')
+    success_message = _('Status successfully deleted')
     login_url = 'login'
