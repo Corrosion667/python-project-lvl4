99f040efce6d009c333073342fcf650a94ebfef0
Artem Nechai <lenikol95@mail.ru>
2022-01-21 20:02:29 +0300

    create filter for tasks

---
+++ task_manager/tasks/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+
+from task_manager.tasks.models import Task
+
+
+class TaskFilter (django_filters.FilterSet):
+    class Meta:
+        model = Task
+        fields = [
+            'status',
+            'executor',
+            'labels',
+        ]
