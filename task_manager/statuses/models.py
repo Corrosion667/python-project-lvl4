50e9aff04e9688f02b935b9da5051b3003d85317
Artem Nechai <lenikol95@mail.ru>
2022-01-23 21:03:09 +0300

    update localize

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -10,7 +10,7 @@ class Status(models.Model):
     """Status of the task."""
 
     name = models.CharField(
-        verbose_name=_('Name'),
+        verbose_name=_('AAAA'),
         max_length=MAX_LENGTH_OF_STATUS_NAME,
         unique=True,
     )
