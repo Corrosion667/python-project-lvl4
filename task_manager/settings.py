e9c3043e8e72d512af0c62072a5a699ecbe48a0f
Артём Нечай <a.nechai@okenit.ru>
2022-03-22 10:41:32 +0300

    change env naming

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -12,15 +12,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-
-CSRF_TRUSTED_ORIGINS = os.getenv(
-    'CSRF_TRUSTED_ORIGINS', default='http://webserver:9000',
-).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
